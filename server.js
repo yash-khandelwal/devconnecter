1de4d8aea4c6c2c74cf1722bbaf06a0bb4e717ee
yash-khandelwal <kritagya.0398@gmail.com>
2020-12-30 20:42:00 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express  = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("api running");
+})
+
+app.listen(PORT, () => {
+    console.log('server is running at ', PORT);
+})
